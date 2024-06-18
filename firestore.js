bda63c323e21fcbe76b1ef875021e5500a3a9c5e
SeniorHS <hernando.salazar@inacapmail.cl>
2024-06-17 23:19:11 -0400

    first commit

---
+++ firestore.js
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
+import { addDoc, collection, deleteDoc, doc, getDoc, getFirestore, onSnapshot, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
+// TODO: Documentación
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBmmMt1lLpigl6IqCA1piN01L0_DbO7gJ8",
+    authDomain: "nutre-f.firebaseapp.com",
+    projectId: "nutre-f",
+    storageBucket: "nutre-f.appspot.com",
+    messagingSenderId: "638708490676",
+    appId: "1:638708490676:web:dc9880e20cd3852f9d52e1",
+    measurementId: "G-HXBCJQ8C83"
+}
+
+// Initialize Firebase
+const HealthyLife = initializeApp(firebaseConfig)
+//Función de firestore que permite retornar la base de datos para su utilización
+const db = getFirestore(HealthyLife)
+
+//función para guardar los datos en firestore
+export const save = (persona) => {
+    //addDoc es la función de firestore que permite añadir un nuevo documento
+    //collection es la función de firestore que permite traer la colección de la db
+    addDoc(collection(db, 'Persona'), persona)
+}
+
+//función que permite obtener la colección 
+export const getData = (data) => {
+    //onSnapshot permite retornar la colleción y asignarla a la variable data 
+    onSnapshot(collection(db, 'Persona'), data)
+}
+
+//función remove, permite eliminar un registro según su id
+export const remove = (id) => {
+    //deleteDoc es una función de firestore que permite quitar un documento de la colección
+    //doc es una función de firestore que permite buscar un documento por su id
+    deleteDoc(doc(db, 'Persona', id))
+}
+
+//función getDocument nos permite obtener un documento según su id 
+//getDoc permite traer un documento según si id y acceder a sus valores
+export const getDocumento = (id) => getDoc(doc(db, 'Persona', id))
+
+//función update permite editar un documento
+export const update = (id, emp) => {
+    //updateDoc es una funcioón de firestore que permite modificar un documento
+    updateDoc(doc(db, 'Persona', id), emp)
+}
---
+++ utilidades.js
@@ -0,0 +1,56 @@
+
+const limpiar = () => {
+    document.querySelector('form').reset()
+    document.querySelectorAll('.form-control').forEach(item => {
+        item.classList.remove('is-invalid')
+        item.classList.remove('is-valid')
+        document.getElementById('p-' + item.name).innerHTML = ''
+    })
+    document.getElementById('runfloat').readOnly = false
+    document.getElementById('btnGuardar').value = 'Guardar'
+}
+
+const verificar = (id) => {
+    //getElementById(id) obtiene el elemento html por su id
+    const input = document.getElementById(id)
+    const div = document.getElementById('p-' + id)
+    //removemos el estilo invalid
+    input.classList.remove('is-invalid')
+    //verificamos si valor del input es vacío
+    if (input.value.trim() == '') {
+        //classList permite añadir o quitar clases a un elemento HTML (add para añadir y remove para quitar)
+        input.classList.add('is-invalid') //is-invalid es clase de bootstrap que deja en rojo el input
+        //innerHTML permite agregar elementos html desde js
+        div.innerHTML = '<span class="badge bg-danger">El campo es obligatorio</span>'
+    } else {
+        input.classList.add('is-valid') //is-valid es claase bootstrap que deja el input en verde
+        div.innerHTML = ''
+        if (id == 'runfloat') {
+            if (!validaRun(input.value.trim())) {
+                input.classList.add('is-invalid')
+                div.innerHTML = '<span class="badge bg-danger">El run no es válido</span>'
+            }
+        }
+        if (id == 'emailfloat') {
+            if (!validarEmail(input.value)) {
+                input.classList.add('is-invalid')
+                div.innerHTML = '<span class="badge bg-danger">El email no tiene el formato correcto</span>'
+            }
+        }
+    }
+}
+
+const soloNumeros = (p) => {
+    if (p.keyCode >= 48 && p.keyCode <= 57)
+        return true //true permitira ver la tecla en el input
+    return false //false no deja ver la tecla
+}
+
+const validarEmail = (emailfloat) => {
+    //expresión regular del formato email
+    const formato = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/
+    //vertifica si el email enviado no tiene el formato correcto
+    if (!formato.test(emailfloat))
+        return false //si retorna falso no es válido
+    return true
+}
